70995bb9eb9ca1c0f68a9839c7411dc414f55b82
SSoledade <soledade.contato@gmail.com>
2021-10-05 10:22:42 -0300

    organização do css

--- beta/includes/footer.php
+++ beta/includes/footer.php
@@ -1,22 +1,9 @@
-<!doctype html>
-<html lang="pt-br">
+<footer>
+    footer de todas paginas
+</footer>
 
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="../css/index.css">
-
-    <title>Hello, world!</title>
-</head>
-
-<body>
-    <h1>Hello, world!</h1>
-
-    <!-- bootstrap js -->
-    <script src="../js/bootstrap.min.js"></script>
+<!-- bootstrap js -->
+<script src="../js/bootstrap.min.js"></script>
 
 </body>
 
---
+++ beta/includes/header.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../css/saimon/index.css">
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+    <header>
+        Nav de todas paginas
+    </header>
