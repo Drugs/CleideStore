abf3ff785e194e59abb1cca3b32dc1c6d0ec1eb0
SSoledade <soledade.contato@gmail.com>
2021-10-04 21:53:58 -0300

    reorganizacao de arquivos

---
+++ beta/includes/footer.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../css/index.css">
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+    <h1>Hello, world!</h1>
+
+    <!-- bootstrap js -->
+    <script src="../js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
