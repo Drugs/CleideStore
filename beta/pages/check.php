dc76db7bfdeae3646ce607d124bb41eb9bdc7814
Drugs <israelmadrugaps@gmail.com>
2021-11-09 20:21:15 -0300

    correção check

--- beta/pages/check.php
+++ beta/pages/check.php
@@ -1,64 +1,15 @@
 <?php
+
 include "../includes/header_dash.php";
 include '../database/Connection.php';
 session_start();
 ?>
-
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.88.1">
-    <title>Checkout example · Bootstrap v5.1</title>
-
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/checkout/">
-
-    
-
-    <!-- Bootstrap core CSS -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
-
-    <!-- Favicons -->
-<link rel="apple-touch-icon" href="/docs/5.1/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
-<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
-<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
-<link rel="manifest" href="/docs/5.1/assets/img/favicons/manifest.json">
-<link rel="mask-icon" href="/docs/5.1/assets/img/favicons/safari-pinned-tab.svg" color="#7952b3">
-<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon.ico">
-<meta name="theme-color" content="#7952b3">
-
-
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
-
-    
-    <!-- Custom styles for this template -->
-    <link href="../css/form-validation.css" rel="stylesheet">
-  </head>
-  <body class="bg-light">
     
     <div class="container d-flex justfy col-md-8">
-    <h4 class="col-mb-3">JÁ TEM LOGIN?</h4>
-    <a class="w-5 btn btn-sm btn-outline-secondary" href="./login.php">Faça login</a><br><br>
-
-      <div class="col-md-5 col-lg-4 order-md-last">
-      </div>
+		<div class="row">
+			<h4 class="col-mb-3">JÁ TEM LOGIN?</h4>
+			<a class="w-5 btn btn-sm btn-outline-secondary" href="./login.php">Faça login</a>
+		</div>
       <div class="col-md-7 col-lg-8">
         <h4 class="mb-3">QUERO ME CADASTRAR</h4>
         <form class="needs-validation" action="" novalidate>
@@ -152,6 +103,6 @@ session_start();
 
     <script src="../js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
 
-      <script src="form-validation.js"></script>
+      <script src="../js/form-validation.js"></script>
   </body>
 </html>
