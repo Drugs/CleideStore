892be64d35ed10ca7d4826ae42c42aa3191c9d92
Júnior Coelho <90370668+Cirilo-Junior@users.noreply.github.com>
2021-11-09 20:34:50 -0300

    comit

--- beta/pages/check.php
+++ beta/pages/check.php
@@ -1,57 +1,8 @@
 <?php
-include "../includes/header_dash.php";
+
 include '../database/Connection.php';
 session_start();
 ?>
-
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.88.1">
-    <title>Checkout example · Bootstrap v5.1</title>
-
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/checkout/">
-
-    
-
-    <!-- Bootstrap core CSS -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
-
-    <!-- Favicons -->
-<link rel="apple-touch-icon" href="/docs/5.1/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
-<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
-<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
-<link rel="manifest" href="/docs/5.1/assets/img/favicons/manifest.json">
-<link rel="mask-icon" href="/docs/5.1/assets/img/favicons/safari-pinned-tab.svg" color="#7952b3">
-<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon.ico">
-<meta name="theme-color" content="#7952b3">
-
-
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
-
-    
-    <!-- Custom styles for this template -->
-    <link href="../css/form-validation.css" rel="stylesheet">
-  </head>
-  <body class="bg-light">
     
     <div class="container d-flex justfy col-md-8">
     <h4 class="col-mb-3">JÁ TEM LOGIN?</h4>
@@ -89,12 +40,11 @@ session_start();
 
             <div class="col-12">
               <label for="password" class="form-label">Senha <span class="text-muted"></span></label>
-              <input type="password" class="form-control" id="email" placeholder="">
+              <input type="password" class="form-control" id="senha" placeholder="">
               <div class="invalid-feedback">
                Por favor insira um email válido.
               </div>
             </div>
-
             <div class="col-12">
               <label for="address" class="form-label">Endereço</label>
               <input type="text" class="form-control" id="address" placeholder="1234 Main St" required>
@@ -110,7 +60,7 @@ session_start();
 
             <div class="col-md-5">
             <label for="address2" class="form-label">Cidade<span class="text-muted"></span></label>
-              <input type="text" class="form-control" id="address2" placeholder="">
+              <input type="text" class="form-control" id="address3" placeholder="">
                 
                 
               </select>
@@ -121,7 +71,7 @@ session_start();
 
             <div class="col-md-4">
             <label for="address2" class="form-label">Estado<span class="text-muted"></span></label>
-              <input type="text" class="form-control" id="address2" placeholder="">
+              <input type="text" class="form-control" id="address4" placeholder="">
               <div class="invalid-feedback">
                 Please provide a valid state.
               </div>
@@ -152,6 +102,6 @@ session_start();
 
     <script src="../js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
 
-      <script src="form-validation.js"></script>
+      <script src="../js/form-validation.js"></script>
   </body>
 </html>
